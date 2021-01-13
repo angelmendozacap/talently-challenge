3325bb182c5492e8045293a0f117b05d5a6b79aa
Alfredo Mendoza <1930.kick@gmail.com>
2021-01-13 12:07:08 -0500

    Setting up Entities

---
+++ app/Http/Controllers/WorkApplicationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkApplication;
+use Illuminate\Http\Request;
+
+class WorkApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WorkApplication  $workApplication
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WorkApplication $workApplication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WorkApplication  $workApplication
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WorkApplication $workApplication)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WorkApplication  $workApplication
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WorkApplication $workApplication)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WorkApplication  $workApplication
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WorkApplication $workApplication)
+    {
+        //
+    }
+}
